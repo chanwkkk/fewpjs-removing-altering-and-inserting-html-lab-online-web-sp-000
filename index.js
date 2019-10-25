b1c4b46b154fa9b87ddc2eb2e37dc5474280e4a1
Yingqi Chen <chanwk18@hotmail.com>
2019-10-25 08:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 // Write your code here!
-let element = document.createElement('div')
+document.querySelector("main").remove()
+
+
+let h1 = document.createElement('h1')
 
 document.body.appendChild(element)
 
-let ul = document.createElement('ul')
 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
-
-element.appendChild(ul)
